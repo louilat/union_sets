42c2707b1c008ad0168ad7c310355b518c3aa30b
louislat <lmm.latournerie@gmail.com>
2024-10-15 19:48:09 +0200

    format

--- main.jl
+++ main.jl
@@ -2,10 +2,12 @@ using Printf
 include("src/utils.jl")
 include("src/union_set_algorithm.jl")
 
+
 # Parameters
 unionset::UnionSet = UnionSet(Int32(2), Int32(500), Int32(500))
 const epsilon::Float32 = 1 / 100
 const delta::Float32 = 1 / 100
+
 
 h::Float32 = approximate_union_set(unionset, epsilon = epsilon, delta = delta)
 println("approximation h = $h")
